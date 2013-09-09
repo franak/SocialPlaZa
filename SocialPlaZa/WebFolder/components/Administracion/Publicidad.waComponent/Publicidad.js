d0bff2eef507a36c479a904d3152f35369393753
dsantiago89 <dsantiago@telegestion.com>
2013-09-09 15:00:19 +0200

    ds

--- SocialPlaZa/WebFolder/components/Administracion/Publicidad.waComponent/Publicidad.js
+++ SocialPlaZa/WebFolder/components/Administracion/Publicidad.waComponent/Publicidad.js
@@ -29,10 +29,64 @@ $(':input').bind('blur',function() {
 
  
 	// @region namespaceDeclaration// @startlock
+	var button6 = {};	// @button
+	var button5 = {};	// @button
+	var button9 = {};	// @button
+	var button8 = {};	// @button
+	var button7 = {};	// @button
+	var dataGrid1 = {};	// @dataGrid
 	var button1 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	button6.click = function button6_click (event)// @startlock
+	{// @endlock
+		sources.publicidad.serverRefresh({
+			onSuccess:function(){
+				$$(id+"_dialog1").closeDialog();
+			}
+		});
+		
+	};// @lock
+
+	button5.click = function button5_click (event)// @startlock
+	{// @endlock
+		sources.publicidad.save({
+			onSuccess:function(){
+				sources.publicidad.serverRefresh();
+				$$(id+"_dialog1").closeDialog();
+			}
+		});
+	};// @lock
+
+	button9.click = function button9_click (event)// @startlock
+	{// @endlock
+		sources.publicidad.removeCurrent();
+	};// @lock
+
+	button8.click = function button8_click (event)// @startlock
+	{// @endlock
+		$("body").append($("#"+id+"_dialog1"));
+		$("#"+id+"_dialog1").css("top","20px");
+		$$(id+"_dialog1").displayDialog();
+	};// @lock
+
+	button7.click = function button7_click (event)// @startlock
+	{// @endlock
+		sources.publicidad.newEntity();
+		$("body").append($("#"+id+"_dialog1"));
+		$("#"+id+"_dialog1").css("top","20px");
+		$$(id+"_dialog1").displayDialog();
+		
+	};// @lock
+
+	dataGrid1.onRowDblClick = function dataGrid1_onRowDblClick (event)// @startlock
+	{// @endlock
+		$("body").append($("#"+id+"_dialog1"));
+		$("#"+id+"_dialog1").css("top","20px");
+		$$(id+"_dialog1").displayDialog();
+	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
@@ -40,6 +94,12 @@ $(':input').bind('blur',function() {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button6", "click", button6.click, "WAF");
+	WAF.addListener(this.id + "_button5", "click", button5.click, "WAF");
+	WAF.addListener(this.id + "_button9", "click", button9.click, "WAF");
+	WAF.addListener(this.id + "_button8", "click", button8.click, "WAF");
+	WAF.addListener(this.id + "_button7", "click", button7.click, "WAF");
+	WAF.addListener(this.id + "_dataGrid1", "onRowDblClick", dataGrid1.onRowDblClick, "WAF");
 	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
 	// @endregion// @endlock
 
--- SocialPlaZa/WebFolder/components/Administracion/Usuarios.waComponent/Usuarios.js
+++ SocialPlaZa/WebFolder/components/Administracion/Usuarios.waComponent/Usuarios.js
@@ -13,11 +13,58 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var button1 = {};	// @button
+	var button7 = {};	// @button
+	var button8 = {};	// @button
+	var button9 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		sources.usuarios.serverRefresh();
+				$$(id+"_dialog1").closeDialog();
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		sources.usuarios.save({
+			onSuccess:function(){
+				sources.usuarios.serverRefresh();
+				$$(id+"_dialog1").closeDialog();
+			}
+		});
+	};// @lock
+
+	button7.click = function button7_click (event)// @startlock
+	{// @endlock
+		sources.usuarios.newEntity();
+		$("body").append($("#"+id+"_dialog1"));
+		$("#"+id+"_dialog1").css("top","20px");
+		$$(id+"_dialog1").displayDialog();
+		
+	};// @lock
+
+	button8.click = function button8_click (event)// @startlock
+	{// @endlock
+		$("body").append($("#"+id+"_dialog1"));
+		$("#"+id+"_dialog1").css("top","20px");
+		$$(id+"_dialog1").displayDialog();
+	};// @lock
+
+	button9.click = function button9_click (event)// @startlock
+	{// @endlock
+		sources.usuarios.removeCurrent();
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
+	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
+	WAF.addListener(this.id + "_button7", "click", button7.click, "WAF");
+	WAF.addListener(this.id + "_button8", "click", button8.click, "WAF");
+	WAF.addListener(this.id + "_button9", "click", button9.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
