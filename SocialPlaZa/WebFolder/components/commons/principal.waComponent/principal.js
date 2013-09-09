ae853f4e2ac79948d80532423efd0942f01740bb
Fran Caja <fcaja@socialerp.net>
2013-09-09 14:58:26 +0200

    Principal

--- SocialPlaZa/WebFolder/components/commons/principal.waComponent/principal.js
+++ SocialPlaZa/WebFolder/components/commons/principal.waComponent/principal.js
@@ -13,11 +13,25 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var container1 = {};	// @container
+	var container3 = {};	// @container
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	container1.click = function container1_click (event)// @startlock
+	{// @endlock
+		
+	};// @lock
+
+	container3.click = function container3_click (event)// @startlock
+	{// @endlock
+		appds.openAdmin();
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_container1", "click", container1.click, "WAF");
+	WAF.addListener(this.id + "_container3", "click", container3.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
